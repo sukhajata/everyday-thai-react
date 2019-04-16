a2201e0708b1013bdd17b99d3daf7fd04c570308
sukhajata <sukhajata@gmail.com>
2019-04-16 16:40:30 +0700

    Added environment variables

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -1,9 +1,9 @@
 module.exports = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    messagingPublicVapidKey: process.env.REACT_APP_PUBLIC_VAPID_KEY
+    apiKey: "AIzaSyCF9PthVik4BrQWPjf14r0Ogs6_RfuixuM",
+    authDomain: "everyday-english-156201.firebaseapp.com",
+    databaseURL: "https://everyday-english-156201.firebaseio.com",
+    projectId: "everyday-english-156201",
+    storageBucket: "everyday-english-156201.appspot.com",
+    messagingSenderId: "360023939896",
+    messagingPublicVapidKey: "BKeKhlfqkcG5L7mCkPmY5pETSJ0RVQhvAvbJXoQXE49V-714TDVM5Kw54QuRSta2PU6iR1BVK1IDaAmkQV9lBOA",
 }
